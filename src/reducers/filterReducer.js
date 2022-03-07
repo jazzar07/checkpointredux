796da0cb09754e8c2f24c9cfbced04afd4099288
jazzer07 <jazzarbassem07@gmail.com>
2022-03-07 17:34:06 +0100

    first commit

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,13 @@
+import { FILTERTODO } from "../actions/types";
+
+const filterReducer = (state = [""], action) => {
+  switch (action.type) {
+    case FILTERTODO:
+      return [(state = action.payload)];
+
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
